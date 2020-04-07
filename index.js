be0b5d0fa4c925d4f1dcd4c284f4812151d4b273
metzz <metzz@dashskins.com.br>
2020-04-07 09:18:46 -0300

    add !cotacao to bot

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ const Discord = require('discord.js')
 const client = new Discord.Client()
 const axios = require('axios')
 const emoji = require('node-emoji')
-
 require('dotenv').config()
+
+
 client.on('ready', () => {
   console.log(`${emoji.emojify('Made w/ :heart:')}\nLogado como: ${client.user.tag}`);
 });
-
+// CORONA
 client.on('message', async msg => {
    if (msg.content.startsWith("!corona")) {
     const pais = msg.content.split(' ')[1]
@@ -45,6 +46,74 @@ client.on('message', async msg => {
           let embed = new Discord.MessageEmbed()
           embed.setTitle(`${emoji.emojify(':large_blue_circle:')} ${pais.toUpperCase()}`)
           embed.setDescription(`\n\nCasos:${response.cases}\nCasos Hoje:${response.todayCases}\nMortes:${response.deaths}\nMortes Hoje: ${response.todayDeaths}`)
+          msg.channel.send(embed)
+          }
+        })
+        .catch(error=> {
+          console.log(error);
+        })
+    }
+  }
+  // COTACAO
+  if (msg.content.startsWith("!cotacao")) {
+    const moeda = msg.content.split(' ')[1]
+    if (moeda === 'ajuda' || typeof moeda === 'undefined') {
+      let embed = new Discord.MessageEmbed()
+      embed.setTitle(`${emoji.emojify(':sos:')} Ajuda ${emoji.emojify(':sos:')}`)
+      embed.setDescription("\n!cotacao (moeda) - Cotacao atual da moeda\nMoedas Disponiveis: Dolar, Euro, Peso, Bitcoin e Libra")
+      msg.channel.send(embed)
+    } 
+
+    else if (msg.content === `!cotacao ${moeda}`) {
+        console.log(moeda)
+      let getData = async () => {
+        let response = await axios.get(`https://api.hgbrasil.com/finance?key=${process.env.HG_KEY}/`)
+        .then(response => {
+          return response
+        })
+        .catch(error=> {
+          console.log(error)
+          return error
+        })
+        return response.data
+      }
+      await getData ()
+        .then(response=> {
+          if (moeda.toLocaleLowerCase() === 'dolar') {
+            let embed = new Discord.MessageEmbed()
+            embed.setTitle(`${emoji.emojify(':currency_exchange:')} Dolar (USD)!`)
+            embed.setDescription(`Cotacao do Dolar: R$${response.results.currencies.USD.buy}`)
+            msg.channel.send(embed)
+          } else if (moeda.toLocaleLowerCase() === 'libra') {
+            let embed = new Discord.MessageEmbed()
+            embed.setTitle(`${emoji.emojify(':currency_exchange:')} Libra (GBP)!`)
+            embed.setDescription(`Cotacao da Libra: R$${response.results.currencies.GBP.buy}`)
+            msg.channel.send(embed)
+          }
+          else if (moeda.toLocaleLowerCase() === 'peso') {
+            let embed = new Discord.MessageEmbed()
+            embed.setTitle(`${emoji.emojify(':currency_exchange:')} Peso Argentino (ARS)!`)
+            embed.setDescription(`Cotacao do Peso: R$${response.results.currencies.ARS.buy}`)
+            msg.channel.send(embed)
+          }
+          else if(moeda.toLocaleLowerCase() === 'euro') {
+            let embed = new Discord.MessageEmbed()
+            embed.setTitle(`${emoji.emojify(':currency_exchange:')} Euro (EUR)!`)
+            embed.setDescription(`Cotacao do Euro: R$${response.results.currencies.EUR.buy}`)
+            msg.channel.send(embed)
+          }
+          else if (moeda.toLocaleLowerCase() === 'bitcoin') {
+            let embed = new Discord.MessageEmbed()
+            embed.setTitle(`${emoji.emojify(':currency_exchange:')} Bitcoin (BTC)!`)
+            embed.setDescription(`Cotacao do Bitcoin: R$${response.results.currencies.BTC.buy}`)
+            msg.channel.send(embed)
+          }
+        
+          else {
+         console.log(`Erro moeda inexistente`)
+          let embed = new Discord.MessageEmbed()
+          embed.setTitle(`${emoji.emojify(':red_circle:')} ERRO!`)
+          embed.setDescription(`A moeda: ${moeda} nao esta disponivel`)
           msg.channel.send(embed)
           }
         })
