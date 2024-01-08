eadf2df5a0b0cf54cdfa8dae5a5885122256dde7
metzz <54819594+metzz1@users.noreply.github.com>
2024-01-08 09:14:01 -0800

    refactor: improve code readability

--- index.js
+++ index.js
@@ -4,125 +4,95 @@ const axios = require('axios')
 const emoji = require('node-emoji')
 require('dotenv').config()
 
+const discordMessage = (title, description) => {
+  let embed = new Discord.MessageEmbed()
+  embed.setTitle(title)
+  embed.setDescription(description)
+  return embed
+}
+
+const fetchData = async url => {
+  try {
+    const response = await axios.get(url)
+    return response?.data
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+const currencyEmbed = (title, currency) => {
+  const dTitle = `${emoji.emojify(':currency_exchange:')} ${title} (${currency})!`
+  const dDescription = `Cotacao do ${title}: R$${response.results.currencies[currency].buy}`
+  return discordMessage(dTitle, dDescription)
+}
 
 client.on('ready', () => {
-  console.log(`${emoji.emojify('Made w/ :heart:')}\nLogado como: ${client.user.tag}`);
-});
+  console.log(`${emoji.emojify('Made w/ :heart:')}\nLogado como: ${client.user.tag}`)
+})
 // CORONA
 client.on('message', async msg => {
-   if (msg.content.startsWith("!corona")) {
-    const pais = msg.content.split(' ')[1]
-    if (pais === 'ajuda' || typeof pais === 'undefined') {
-      let embed = new Discord.MessageEmbed()
-      embed.setTitle(`${emoji.emojify(':sos:')} Ajuda ${emoji.emojify(':sos:')}`)
-      embed.setDescription("\n!corona (nome do pais em ingles) - Informacoes de um pais\n!corona world - Informacoes do mundo inteiro")
-      msg.channel.send(embed)
-    } 
-
-    else if (msg.content === `!corona ${pais}`) {
-        console.log(pais)
-      let getData = async () => {
-        let response = await axios.get(`https://coronavirus-19-api.herokuapp.com/countries/${pais}`)
-        .then(response => {
-          return response
-        })
-        .catch(error=> {
-          console.log(error)
-          return error
-        })
-        return response.data
-      }
-      await getData ()
-        .then(response=> {
-          console.log(response)
-          if (response.cases === undefined) {
-            let embed = new Discord.MessageEmbed()
-            embed.setTitle(`${emoji.emojify(':red_circle:')} Erro!`)
-            embed.setDescription("Pais inexistente ou indisponivel")
-            msg.channel.send(embed)
-          }
-          else {
-         console.log(`Data from ${pais.toUpperCase()} obtained`)
-          let embed = new Discord.MessageEmbed()
-          embed.setTitle(`${emoji.emojify(':large_blue_circle:')} ${pais.toUpperCase()}`)
-          embed.setDescription(`\n\nCasos: ${response.cases.toLocaleString('de-DE')}\nCasos Hoje: ${response.todayCases.toLocaleString('de-DE')}\nMortes: ${response.deaths.toLocaleString('de-DE')}\nMortes Hoje: ${response.todayDeaths.toLocaleString('de-DE')}`)
-          msg.channel.send(embed)
-          }
-        })
-        .catch(error=> {
-          console.log(error);
-        })
+  if (msg.content.startsWith('!corona')) {
+    const pais = msg.content?.split(' ')?.pop()
+    if (pais === 'ajuda' || !pais) {
+      return msg.channel.send(
+        discordMessage(
+          `${emoji.emojify(':sos:')} Ajuda ${emoji.emojify(':sos:')}`,
+          '\n!corona (nome do pais em ingles) - Informacoes de um pais\n!corona world - Informacoes do mundo inteiro'
+        )
+      )
     }
+    const coronaData = await fetchData(`https://coronavirus-19-api.herokuapp.com/countries/${pais}`)
+    if (!response.cases || !coronaData)
+      return msg.channel.send(discordMessage(`${emoji.emojify(':red_circle:')} Erro!`, 'Pais inexistente ou indisponivel'))
+    return msg.channel.send(
+      discordMessage(
+        `${emoji.emojify(':large_blue_circle:')} ${pais.toUpperCase()}`,
+        `\n\nCasos: ${response.cases.toLocaleString('de-DE')}\nCasos Hoje: ${response.todayCases.toLocaleString(
+          'de-DE'
+        )}\nMortes: ${response.deaths.toLocaleString('de-DE')}\nMortes Hoje: ${response.todayDeaths.toLocaleString('de-DE')}`
+      )
+    )
   }
   // COTACAO
-  if (msg.content.startsWith("!cotacao")) {
-    const moeda = msg.content.split(' ')[1]
-    if (moeda === 'ajuda' || typeof moeda === 'undefined') {
-      let embed = new Discord.MessageEmbed()
-      embed.setTitle(`${emoji.emojify(':sos:')} Ajuda ${emoji.emojify(':sos:')}`)
-      embed.setDescription("\n!cotacao (moeda) - Cotacao atual da moeda\nMoedas Disponiveis: Dolar, Euro, Peso, Bitcoin e Libra")
-      msg.channel.send(embed)
-    } 
-
-    else if (msg.content === `!cotacao ${moeda}`) {
-        console.log(moeda)
-      let getData = async () => {
-        let response = await axios.get(`https://api.hgbrasil.com/finance?key=${process.env.HG_KEY}/`)
-        .then(response => {
-          return response
-        })
-        .catch(error=> {
-          console.log(error)
-          return error
-        })
-        return response.data
-      }
-      await getData ()
-        .then(response=> {
-          if (moeda.toLocaleLowerCase() === 'dolar') {
-            let embed = new Discord.MessageEmbed()
-            embed.setTitle(`${emoji.emojify(':currency_exchange:')} Dolar (USD)!`)
-            embed.setDescription(`Cotacao do Dolar: R$${response.results.currencies.USD.buy}`)
-            msg.channel.send(embed)
-          } else if (moeda.toLocaleLowerCase() === 'libra') {
-            let embed = new Discord.MessageEmbed()
-            embed.setTitle(`${emoji.emojify(':currency_exchange:')} Libra (GBP)!`)
-            embed.setDescription(`Cotacao da Libra: R$${response.results.currencies.GBP.buy}`)
-            msg.channel.send(embed)
-          }
-          else if (moeda.toLocaleLowerCase() === 'peso') {
-            let embed = new Discord.MessageEmbed()
-            embed.setTitle(`${emoji.emojify(':currency_exchange:')} Peso Argentino (ARS)!`)
-            embed.setDescription(`Cotacao do Peso: R$${response.results.currencies.ARS.buy}`)
-            msg.channel.send(embed)
-          }
-          else if(moeda.toLocaleLowerCase() === 'euro') {
-            let embed = new Discord.MessageEmbed()
-            embed.setTitle(`${emoji.emojify(':currency_exchange:')} Euro (EUR)!`)
-            embed.setDescription(`Cotacao do Euro: R$${response.results.currencies.EUR.buy}`)
-            msg.channel.send(embed)
-          }
-          else if (moeda.toLocaleLowerCase() === 'bitcoin') {
-            let embed = new Discord.MessageEmbed()
-            embed.setTitle(`${emoji.emojify(':currency_exchange:')} Bitcoin (BTC)!`)
-            embed.setDescription(`Cotacao do Bitcoin: R$${response.results.currencies.BTC.buy}`)
-            msg.channel.send(embed)
-          }
-        
-          else {
-         console.log(`Erro moeda inexistente`)
-          let embed = new Discord.MessageEmbed()
-          embed.setTitle(`${emoji.emojify(':red_circle:')} ERRO!`)
-          embed.setDescription(`A moeda: ${moeda} nao esta disponivel.\nPara checar as moedas disponiveis digite: !cotacao`)
-          msg.channel.send(embed)
-          }
-        })
-        .catch(error=> {
-          console.log(error);
-        })
+  if (msg.content.startsWith('!cotacao')) {
+    const moeda = msg.content?.split(' ')?.pop()
+    if (moeda === 'ajuda' || !moeda) {
+      return msg.channel.send(
+        discordMessage(
+          `${emoji.emojify(':sos:')} Ajuda ${emoji.emojify(':sos:')}`,
+          '\n!cotacao (moeda) - Cotacao atual da moeda\nMoedas Disponiveis: Dolar, Euro, Peso, Bitcoin e Libra'
+        )
+      )
+    }
+    const currencyData = await fetchData(`https://api.hgbrasil.com/finance?key=${process.env.HG_KEY}/`)
+    const values = currencyData?.results?.currencies
+    if (values) return
+    const { USD, GBP, ARS, EUR, BTC } = values
+    switch (moeda.toLocaleLowerCase()) {
+      case 'dolar':
+        msg.channel.send(currencyEmbed('Dolar', 'USD'))
+        break
+      case 'libra':
+        msg.channel.send(currencyEmbed('Libra', 'GBP'))
+        break
+      case 'peso':
+        msg.channel.send(currencyEmbed('Peso Argentino', 'ARS'))
+        break
+      case 'euro':
+        msg.channel.send(currencyEmbed('Euro', 'EUR'))
+        break
+      case 'bitcoin':
+        msg.channel.send(currencyEmbed('Bitcoin', 'BTC'))
+        break
+      default:
+        msg.channel.send(
+          discordMessage(
+            `${emoji.emojify(':red_circle:')} ERRO!`,
+            `A moeda: ${moeda} nao esta disponivel.\nPara checar as moedas disponiveis digite: !cotacao`
+          )
+        )
     }
   }
-    
-    }
-    )
-client.login(process.env.DISCORD_KEY);
+})
+
+client.login(process.env.DISCORD_KEY)
